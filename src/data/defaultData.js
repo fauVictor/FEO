b4c67f9ece09f7e32e9312b14ecdb016225a4c86
victor fau <fau.victor@outlook.com>
2021-07-16 09:40:01 +0200

    Insert default datas

---
+++ src/data/defaultData.js
@@ -0,0 +1,85 @@
+export default
+[
+    {
+        question: 'To be or not to be ?',
+        answers: [
+            {
+                answer: 'yes',
+                vote: 50
+            },
+            {
+                answer: 'No',
+                vote: 15
+            },
+            {
+                answer: 'yup',
+                vote: 8
+            },
+            {
+                answer: 'yeah',
+                vote: 2
+            },
+            {
+                answer: 'Maybe',
+                vote: 14
+            },
+            {
+                answer: 'yup',
+                vote: 10
+            },
+            {
+                answer: 'yeah',
+                vote: 5
+            },
+            {
+                answer: 'yup',
+                vote: 4
+            },
+            {
+                answer: 'yeah',
+                vote: 2
+            },
+        ]
+    },
+    {
+        question: 'To be or not to be ?',
+        answers: [
+            {
+                answer: 'yes',
+                vote: 50
+            },
+            {
+                answer: 'No',
+                vote: 15
+            },
+            {
+                answer: 'yup',
+                vote: 8
+            },
+            {
+                answer: 'yeah',
+                vote: 2
+            },
+            {
+                answer: 'Maybe',
+                vote: 14
+            },
+            {
+                answer: 'yup',
+                vote: 10
+            },
+            {
+                answer: 'yeah',
+                vote: 5
+            },
+            {
+                answer: 'yup',
+                vote: 4
+            },
+            {
+                answer: 'yeah',
+                vote: 2
+            },
+        ]
+    },
+]
